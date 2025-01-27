70f2bd7db533eb4ab952a7a05b2b98f6600d37fa
Leonardo Vinicius Wanderley Jatobá da Silva <lvwjs@ic.ufal.br>
2025-01-26 22:09:01 -0300

    fix filter in next and previous

--- app/views.py
+++ app/views.py
@@ -24,24 +24,53 @@ def movie_detail(request, movie_id):
 def tv_list(request):
     content_type = request.GET.get('type', 'tv')
     movie_service = MovieService()
+    
+    order_by = request.GET.get('order', 'desc')
+    sort_by = request.GET.get('sort_by', 'popularity')
+    search_query = request.GET.get('search', '')
+    page = int(request.GET.get('page', 1))
+    
     context = movie_service.fetch_movies(
-        order_by=request.GET.get('order', 'desc'),
-        sort=request.GET.get('sort_by', 'popularity'),
-        search_query=request.GET.get('search', ''),
-        page=int(request.GET.get('page', 1)),
+        order_by=order_by,
+        sort=sort_by,
+        search_query=search_query,
+        page=page,
         content_type=content_type,
     )
+    
+    context.update({
+        'order': order_by,
+        'sort_by': sort_by,
+        'search_query': search_query,
+        'page': page,
+        'content_type': content_type
+    })
+    
     return render(request, f"list_{content_type}.html", context)
 
 def movie_list(request):
     content_type = request.GET.get('type', 'movie')
     movie_service = MovieService()
+    
+    order_by = request.GET.get('order', 'desc')
+    sort_by = request.GET.get('sort_by', 'popularity')
+    search_query = request.GET.get('search', '')
+    page = int(request.GET.get('page', 1))
+    
     context = movie_service.fetch_movies(
-        order_by=request.GET.get('order', 'desc'),
-        sort=request.GET.get('sort_by', 'popularity'),
-        search_query=request.GET.get('search', ''),
-        page=int(request.GET.get('page', 1)),
+        order_by=order_by,
+        sort=sort_by,
+        search_query=search_query,
+        page=page,
         content_type=content_type,
     )
-
+    
+    context.update({
+        'order': order_by,
+        'sort_by': sort_by,
+        'search_query': search_query,
+        'page': page,
+        'content_type': content_type
+    })
+    
     return render(request, f"list_{content_type}.html", context)
